df540afe3421ad6361bc53d08d2b9be3d2bfb09d
unknown <your.email@example.com>
2026-02-17 22:30:53 +0530

    Create minimal waitlist without complex dependencies

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,7 @@
-import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/not-found";
-import HomeWorking from "@/pages/Home-working";
-
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={HomeWorking} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
+import HomeMinimal from "@/pages/Home-minimal";
 
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Router />
-      </TooltipProvider>
-    </QueryClientProvider>
-  );
+  return <HomeMinimal />;
 }
 
 export default App;
